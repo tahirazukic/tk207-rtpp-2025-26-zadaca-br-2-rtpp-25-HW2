de830e7005b89c144713f89ce0ac87e6c222ffff
Tahira <taha.zuka@gmail.com>
2026-05-09 01:15:49 +0200

    routes_a: implementirana GET metoda koja podržava barem jedan query parametar

--- routes_a.py
+++ routes_a.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from turtle import title
+from typing import List, Optional
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 from models_a import Album, AlbumCreate, AlbumUpdate
@@ -13,3 +17,31 @@ def create_album(album: AlbumCreate, session: Session = Depends(get_session)):
     session.refresh(new_db_album)
     return new_db_album
 
+@router.get("/", response_model=List[Album])
+def read_albums(
+                session=Depends(get_session),
+                title: Optional[str] = None,
+                artist: Optional[str] = None,
+                release_year: Optional[int] = None,
+                price: Optional[float] = None,
+                is_available: Optional[bool] = None
+                world_premiere: Optional[datetime.date] = None
+):
+query_params={
+    "title": title,
+    "artist": artist,
+    "release_year": release_year,
+    "price": price,
+    "is_available": is_available,
+    "world_premiere": world_premiere
+}
+    query = select(Album)
+    for key, value in query_params.items():
+        if value is not None:
+            query = query.where(getattr(Album, key) == value)
+    albums = session.exec(query).all()
+    return albums
+
+
+
+   
