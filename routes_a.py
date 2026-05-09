662d5654689869198a50ed35fd1a43d1d989e556
Tahira <taha.zuka@gmail.com>
2026-05-10 00:29:14 +0200

    routes_a: uređen APIRouter

--- routes_a.py
+++ routes_a.py
@@ -7,7 +7,7 @@ from sqlmodel import Session, select
 from models_a import Album, AlbumCreate, AlbumUpdate
 from database import get_session
 
-router = APIRouter(prefix="/resursi_a", tags=["Resurs A"])
+router = APIRouter(prefix="/Albumi", tags=["Resurs A"])
 
 @router.post("/", response_model=Album, status_code=status.HTTP_201_CREATED)
 def create_album(album: AlbumCreate, session: Session = Depends(get_session)):
