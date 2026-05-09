0d37f70ab652e00385115e4db92ba9c33c3307db
Tahira <taha.zuka@gmail.com>
2026-05-09 23:23:18 +0200

    routes_a: implementirane PATCH i DELETE metode

--- routes_a.py
+++ routes_a.py
@@ -1,5 +1,5 @@
-from datetime import datetime
-from turtle import title
+from datetime import date
+
 from typing import List, Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status
@@ -24,17 +24,17 @@ def read_albums(
                 artist: Optional[str] = None,
                 release_year: Optional[int] = None,
                 price: Optional[float] = None,
-                is_available: Optional[bool] = None
-                world_premiere: Optional[datetime.date] = None
+                is_available: Optional[bool] = None,
+                world_premiere: Optional[date] = None
 ):
-query_params={
-    "title": title,
-    "artist": artist,
-    "release_year": release_year,
-    "price": price,
-    "is_available": is_available,
-    "world_premiere": world_premiere
-}
+    query_params={
+        "title": title,
+        "artist": artist,
+        "release_year": release_year,
+        "price": price,
+        "is_available": is_available,
+        "world_premiere": world_premiere
+    }
     query = select(Album)
     for key, value in query_params.items():
         if value is not None:
@@ -50,17 +50,40 @@ def read_album(album_id: int, session: Session = Depends(get_session)):
     return db_album   
 
 @router.put("/{album_id}", response_model=Album)
-def update_album(album_id: int, album_update: AlbumUpdate, session: Session=Depends(get_session)):
+def update_album(album_id: int, album_update: AlbumCreate, session: Session=Depends(get_session)):
     db_album = session.get(Album, album_id)
     if not db_album:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Album nije pronađen")
-    album_data = album_update.model_dump(exclude_unset=True)
+    album_data = album_update.model_dump() 
     for key, value in album_data.items():
         setattr(db_album, key, value)
     session.add(db_album)
     session.commit()
     session.refresh(db_album)
     return db_album
+
+@router.patch("/{album_id}", response_model=Album)
+def partial_update_album(album_id: int, album_update: AlbumUpdate, session: Session=Depends(get_session)):
+    db_album = session.get(Album, album_id)
+    if not db_album:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Album nije pronađen")
+    album_data = album_update.model_dump(exclude_unset=True) 
+    for key, value in album_data.items():
+        setattr(db_album, key, value)
+    session.add(db_album)
+    session.commit()
+    session.refresh(db_album)
+    return db_album
+
+@router.delete("/{album_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_album(album_id: int, session: Session = Depends(get_session)):
+    db_album = session.get(Album, album_id)
+    if not db_album:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Album nije pronađen")
+    session.delete(db_album)
+    session.commit()
+    return None
+
 
 
 
